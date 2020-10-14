d4b3ce97751a02e05f5203c8c97ab3cfc940eae8
Rajanikbabariya <rajanikbabariya@users.noreply.github.com>
2020-10-14 07:46:43 +0530

    project pull

---
+++ BtryPerNotifire.py
@@ -0,0 +1,22 @@
+import psutil
+from plyer import notification
+import time
+
+battery = psutil.sensors_battery()
+
+while (True):
+    percent = battery.percent
+    notification.notify(
+        title="Battery Percentage",
+        message=f"{percent}% Battery remaining",
+        timeout=10
+    )
+    time.sleep(30 * 30)
+    continue
+
+
+
+
+
+
+
